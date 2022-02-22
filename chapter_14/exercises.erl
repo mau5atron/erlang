9d0875f5afeea0fddeb4e2756a95535cd0fdf784
mau5atron <gabrielbetancourt96@outlook.com>
2022-02-22 03:09:13 -0500

    Added notes and exercises for chapter 14.

---
+++ chapter_14/exercises.erl
@@ -0,0 +1,20 @@
+-module(exercises).
+
+% Exercises:
+
+% 1.
+	% Start two nodes on the same host. Look up the manual page for the rpc
+	% module. Perform some remote procedure calls on the two nodes.
+
+% 2. 
+	% Repeat the previous exercise, only with the two nodes on the same LAN
+
+% 3.
+	% Repeat the previous exercise, only with the two nodes on different
+	% networks.
+
+% 4. 
+	% Write YAFS (Yet Another File Server) using the libraries in lib_chan.
+	% You will learn a lot by doing this. Add "bells and whistles" to your
+	% file server.
+
