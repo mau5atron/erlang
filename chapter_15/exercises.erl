17ae68036120262a60fe74bb096eb518ae3fb032
mau5atron <gabrielbetancourt96@outlook.com>
2022-03-10 22:12:42 -0500

    finalized notes for chapter 15 and added exercises

---
+++ chapter_15/exercises.erl
@@ -0,0 +1,17 @@
+-module(exercises).
+
+% Chapter 15 Exercises:
+
+% 1. 
+	% Download the code for a port driver given earlier and test it on your
+	% system.
+
+% 2.
+	% Go to git://github.com/erlang/linked_in_drivers.git. Download the code
+	% for a linked-in driver and test it on your system. The tricky part of
+	% this is finding the correct commands to compile and link the code. If
+	% you fail this exercise, ask for help on the Erlang mailing list.
+
+% 3. See whether you can find an operating system command to discover which
+% CPU your computer has. If you find such a command, write a function that
+% returns your CPU type, using the function os:cmd to call the OS command.
---
+++ chapter_15/ports/example1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int sum(int x, int y);
+int twice(int x);
+
+int main(){
+	return 0;
+}
+
+int sum(int x, int y){
+	return x + y;
+}
+
+int twice(int x){
+	return 2*x;
+}
