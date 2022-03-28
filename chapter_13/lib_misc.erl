ff14c460740b467490dafbb8d8b90536c8c77433
mau5atron <gabrielbetancourt96@outlook.com>
2022-03-27 23:45:29 -0400

    Finished reading chapter 16 and added exercises.

--- chapter_13/lib_misc.erl
+++ chapter_13/lib_misc.erl
@@ -16,3 +16,28 @@ keep_alive(Name, Fun) ->
 	register(Name, Pid = spawn(Fun)),
 	on_exit(Pid, fun(_Why) -> keep_alive(Name, Fun) end).
 
+
+consult(File) ->
+	case file:open(File, read) of
+		{ok, S} ->
+			Val = consult1(S),
+			file:close(S),
+			{ok, Val};
+
+		{error, Why} ->
+			{error, Why}
+	end.
+
+consult1(S) ->
+	case io:read(S, '') of
+		{ok, Term} -> [Term|consult1(S)];
+		eof 			 -> [];
+		Error 		 -> Error
+	end.
+
+
+% writes a list of terms to a file
+unconsult(File, L) ->
+	{ok, S} = file:open(File, write),
+	lists:foreach(fun(X) -> io:format(S, "~p.~n", [X]) end, L),
+	file:close(S).
